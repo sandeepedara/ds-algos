502d34211802759840741d0853b3c5be9847bcd7
sandeepedara <sandeepedara@gmail.com>
2019-08-25 10:55:01 +0530

    recursive - reverse & multiplier

--- recursion-exercies.js
+++ recursion-exercies.js
@@ -34,14 +34,44 @@ function recursiveExponent(base, expo) {
 }
 
 //5
+//[1,2,3] 3
 function recursiveMultiplier(arr, num) {
+    let length = arr.length;
+    function recursive(arr, index) {
+        if(length >=0) {
+            console.log(arr[index]);
+            return;
+        }
+        recursive(arr, index - 1);
+    }
+    recursive(arr, length);
 }
 
+//recursiveMultiplier([1,2,3,4], 3);
 
 function recursiveMultiplier(arr, num) {
-    let newArray = [];
-    arr.forEach((a) => {
-        newArray.push(a * num);
-    });
+    let newArray  = [];
+    function recursive(length) {
+        if (length >= arr.length) {
+            return true;
+        }
+        newArray.push(arr[length] * num || 1) 
+        return recursive(length + 1);
+    }
+    recursive(0);
+    return newArray;
+}
+
+//recursiveReverse([1,2,3,4,5]);
+function recursiveReverse(arr) {
+    let newArray  = [];
+   function reverse(length) {
+        if(length <= 0) {
+            return true;
+        }
+        newArray.push(arr[length - 1])
+       return reverse(length - 1);  
+   }
+    reverse(arr.length);
     return newArray;
 }
